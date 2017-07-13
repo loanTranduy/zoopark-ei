c69ce87b74508e42e6c5c4d77e68a45637a587bf
ingrwf <ingrwf@cpg-429374.cepegra2008.lan>
2017-07-13 10:31:00 +0200

    fin animaux

--- wp-content/themes/supertheme/single-animaux.php
+++ wp-content/themes/supertheme/single-animaux.php
@@ -1,25 +1,29 @@
 <?php get_header(); ?>
 <!-- SINGLE_ANIMAUX -->
     <main>
-        <div class="ask section">
+<div class="parallax-container valign-wrapper">
+        <div class="section no-pad-bot">
             <div class="container">
-            <div class="row">
-                <div class="header-forms col s12 center white-text">
-                    <h3>Les animaux</h3>
-                </div>
-            </div>
+                <!--Parallax 2 content-->
+                    <div class="row center">
+                        <h3 class="header col s12 light">Les animaux</h3>
+                    </div>
             </div>
         </div>
+        <div class="parallax"><img src="<?php the_field('image_principale'); ?>" alt="Unsplashed background img 2"></div>
+    </div>
         <div class="container">
         <div class="col s12 m4">
             <div class="row">
                 <?php  if(have_posts()): the_post();?>
                 <h2><?php the_title(); ?></h2>
+                <!--<div class="animal-image">
+                <img class="hoverZoomLink" src="<?php the_field('image_principale'); ?>" alt="">
+                </div>-->
                 <?php the_content(); ?>
-                <img class="hoverZoomLink" src="<?php the_field('image_couverture'); ?>" alt="">
+                
                 <?php endif; ?>
             </div>
-        </div>
         </div>
     </main>
 <?php  get_footer(); ?>
